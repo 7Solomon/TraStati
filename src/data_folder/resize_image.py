fixed rotation mismatch

src/data_folder/resize_image.py
@@ -19,8 +19,6 @@ def find_zero_size(image):
     
     # Find the indices where the condition is true
     indices = np.argwhere(mask)
-    print(tuple(indices[0]))
-    print(indices.size)
 
     # Return the first matching position, if any
     return tuple(indices[0]) if indices.size > 0 else None
@@ -95,6 +93,9 @@ def create_label_for_cut_images(label_data: dict):
         x, y = element['koordinaten']
         new_point = (ABSTAND + int(x*12), ABSTAND + int(abs(y)*12))
         
+        # Transform for PIL NUMPY missmatch
+        new_point = (new_point[0],new_point[1])
+
         new_element = element.copy()
         new_element['koordinaten'] = new_point
         

src/test.py
@@ -40,14 +40,14 @@ def points_near_line(point_list, x0, y0, x1, y1, threshold_distance):
 def test():
 
     image ,id, label = create_random_image()
-    #points = [_['koordinaten'] for _ in label.values()]
-    #degree_points = [_['rotation'] for _ in label.values()]
-    #degree_lines = get_degree_lines(points, degree_points)
+    points = [_['koordinaten'] for _ in label.values()]
+    degree_points = [_['rotation'] for _ in label.values()]
+    degree_lines = get_degree_lines(points, degree_points)
 
     #print(zero_point)
     img_array = np.array(image)
-    #img = draw_stuff_on_image_and_save(img_array,[(zero_point[1],zero_point[0])],[],point_color=(255,255,0))
-    visualize_image(image)
+    img = draw_stuff_on_image_and_save(img_array,points,degree_lines,point_color=(255,255,0))
+    visualize_image(img)
     
 
 if __name__ == '__main__':